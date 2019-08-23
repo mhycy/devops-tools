383f04ba96faf842d6b3fe3da97f8647053fa39d
mhycy <i@mhycy.me>
2019-08-23 17:48:05 +0800

    fix: message format

--- tools/ddns/ros-aliyun-schedule/index.js
+++ tools/ddns/ros-aliyun-schedule/index.js
@@ -55,9 +55,9 @@ async function updateSubDomainAddress(address) {
     let result = await updateSubDomainAddress(address);
     
     if(result.Status && result.Message) {
-        console.log(`Domain no need update. { Domain: ${Domain}, Address: ${address}`);
+        console.log(`Domain no need update. { Domain: ${Domain}, Address: ${address} }`);
     } else if(result.Status) {
-        console.log(`Address update success. { Domain: ${Domain}, Address: ${address}`);
+        console.log(`Address update success. { Domain: ${Domain}, Address: ${address} }`);
     } else {
         console.log(`Address update fail, need debug.`);
     }
