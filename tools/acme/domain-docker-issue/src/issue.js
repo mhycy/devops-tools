f7626daa0824c42d132d29daf32d1fb2c0384cac
mhycy <i@mhycy.me>
2019-08-26 18:35:56 +0800

    fix bug

--- tools/acme/domain-docker-issue/src/issue.js
+++ tools/acme/domain-docker-issue/src/issue.js
@@ -147,6 +147,7 @@ async function run(tasks, options = {}) {
     
     if(result.renew || forceRunRenewScript || forceRenew) {
       CertInfo = getCertInfomation(task.AcmePath, task.Domain);
+      logger.debug("run::CertInfo", CertInfo);
       await runRenewScript({ ...task, CertInfo });
     }
 
@@ -156,6 +157,7 @@ async function run(tasks, options = {}) {
 
     if(result.renew || forceRunRenewScript || forceRenew) {
       CertInfo = getCertInfomation(task.AcmePath, task.Domain, true);
+      logger.debug("run::CertInfo", CertInfo);
       await runRenewScript({ ...task, CertInfo, EccMode: true });
     }
   }
